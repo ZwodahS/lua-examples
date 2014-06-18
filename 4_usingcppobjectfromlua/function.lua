96f78a1c3574dabe50c75ebfb23594704b0a66c5
Eric Ng(ZwodahS) <ericnjf@gmail.com>
2014-06-18 17:25:44 +0800

    minor fix, make the local variable local

--- 4_usingcppobjectfromlua/function.lua
+++ 4_usingcppobjectfromlua/function.lua
@@ -2,8 +2,12 @@
 -- to target
 function applyDamage(attacker, target)
     print("[Lua] in lua function");
-    damage = attacker:getDamage();
+    local damage = attacker:getDamage();
     print("[Lua] Damage of attacker is " .. damage);
     target:dealtDamage(damage);
     print("[Lua] Damage dealt");
+end
+
+function printDamage()
+    print(damage)
 end
